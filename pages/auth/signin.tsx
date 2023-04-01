21812bef24901d2fbcfccbdb639ae0f8a07a0e27
LeeYongwoo-kor <newlyo1205@naver.com>
2023-04-01 23:59:47 +0900

    Easily implement custom signin pages. 1. redirect to /dashboard if session exists 2. If there is no session, redirect to root

--- pages/auth/signin.tsx
+++ pages/auth/signin.tsx
@@ -1,35 +1,9 @@
-import { GetServerSidePropsContext, InferGetServerSidePropsType } from "next";
+import { GetServerSidePropsContext } from "next";
 import { getServerSession } from "next-auth";
-import { getCsrfToken, getProviders, signIn } from "next-auth/react";
-import styles from "../../styles/Signin.module.css";
 import { authOptions } from "../api/auth/[...nextauth]";
 
-const Signin = ({
-  csrfToken,
-  providers,
-}: InferGetServerSidePropsType<typeof getServerSideProps>) => {
-  return (
-    <div style={{ overflow: "hidden", position: "relative" }}>
-      <div className={styles.wrapper} />
-      <div className={styles.content}>
-        <div className={styles.cardWrapper}>
-          <div className={styles.cardContent}>
-            <input name="csrfToken" type="hidden" defaultValue={csrfToken} />
-            <button className={styles.primaryBtn}>Submit</button>
-            <hr />
-            {providers &&
-              Object.values(providers).map((provider) => (
-                <div key={provider.name} style={{ marginBottom: 0 }}>
-                  <button onClick={() => signIn(provider.id)}>
-                    Sign in with {provider.name}
-                  </button>
-                </div>
-              ))}
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+const Signin = () => {
+  return <div>Processing login...</div>;
 };
 
 export default Signin;
@@ -40,12 +14,5 @@ export async function getServerSideProps(context: GetServerSidePropsContext) {
     return { redirect: { destination: "/dashboard" } };
   }
 
-  const providers = await getProviders();
-  const csrfToken = await getCsrfToken(context);
-  return {
-    props: {
-      providers,
-      csrfToken,
-    },
-  };
+  return { redirect: { destination: "/" } };
 }

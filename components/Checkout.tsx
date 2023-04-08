45b38fc1fe1828d69ab3264efc7cddd14dbca2a1
LeeYongwoo-kor <newlyo1205@naver.com>
2023-04-08 23:59:53 +0900

    Add Checkout Components, Delete Checkout Page

--- components/Checkout.tsx
+++ components/Checkout.tsx
@@ -8,8 +8,8 @@ import {
 } from "@paypal/react-paypal-js";
 
 // This values are the props in the UI
-const amount = "2";
-const currency = "USD";
+const amount = "490";
+const currency = "JPY";
 const style = { layout: "vertical" };
 
 // Custom component to wrap the PayPalButtons and handle currency changes
@@ -50,12 +50,15 @@ const ButtonWrapper = ({ currency, showSpinner }: any) => {
             })
             .then((orderId) => {
               // Your code here after create the order
+              console.log(orderId);
               return orderId;
             });
         }}
         onApprove={function (data, actions) {
+          console.log(data);
           return actions.order.capture().then(function () {
             // Your code here after capture the order
+            console.log("capture!");
           });
         }}
       />
@@ -63,14 +66,14 @@ const ButtonWrapper = ({ currency, showSpinner }: any) => {
   );
 };
 
-export default function App() {
+export default function Checkout() {
   console.log(PAYPAL_CLIENT_ID);
   return (
     <div style={{ maxWidth: "750px", minHeight: "200px" }}>
       <PayPalScriptProvider
         options={{
           "client-id": "test",
-          //   "client-id": PAYPAL_CLIENT_ID!,
+          // "client-id": PAYPAL_CLIENT_ID!,
           components: "buttons",
           currency: "USD",
           locale: "ja_JP",

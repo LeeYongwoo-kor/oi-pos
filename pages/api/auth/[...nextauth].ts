ba847f685c772af1f7cb8d03287817e7e7aed040
LeeYongwoo-kor <newlyo1205@naver.com>
2023-04-11 23:53:29 +0900

    Implement Refresh token rotation using database strategy in next auth (test)

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,7 +1,7 @@
 import prisma from "@/lib/prismadb";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import sgMail from "@sendgrid/mail";
-import NextAuth, { NextAuthOptions } from "next-auth";
+import NextAuth, { NextAuthOptions, TokenSet } from "next-auth";
 import EmailProvider from "next-auth/providers/email";
 import GoogleProvider from "next-auth/providers/google";
 import LineProvider from "next-auth/providers/line";
@@ -39,9 +39,10 @@ export const authOptions: NextAuthOptions = {
       },
     }),
     GoogleProvider({
-      // checks: "both",
       clientId: process.env.GOOGLE_CLIENT_ID!,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+      // use refresh token
+      authorization: { params: { access_type: "offline", prompt: "consent" } },
     }),
     LineProvider({
       clientId: process.env.LINE_CLIENT_ID!,
@@ -61,24 +62,107 @@ export const authOptions: NextAuthOptions = {
       },
     }),
   ],
-  secret: process.env.SECRET,
+  secret: process.env.NEXTAUTH_SECRET,
   session: {
-    strategy: "jwt",
-    // strategy: "database",
-    maxAge: 30 * 60 * 60 * 24,
+    // strategy: "jwt",
+    strategy: "database",
+    maxAge: 30 * 60 * 60 * 24, // 30 days
     // Note: This option is ignored if using JSON Web Tokens
-    updateAge: 60 * 60 * 24,
+    updateAge: 60 * 60 * 24, // 24 hours
   },
   callbacks: {
+    // async jwt({ token, user, account, profile, isNewUser }) {
+    //   console.log("this is jwt callback");
+    //   console.log("token");
+    //   console.log(token);
+    //   console.log("user");
+    //   console.log(user);
+    //   console.log("account");
+    //   console.log(account);
+    //   console.log("profile");
+    //   console.log(profile);
+    //   console.log("isNewUser");
+    //   console.log(isNewUser);
+    //   // Initial sign in
+    //   if (account) {
+    //     token.accessToken = account.access_token;
+    //   }
+    //   return token;
+    // },
+    async session({ session, user, token }: any) {
+      console.log("session");
+      console.log(session);
+      console.log("user");
+      console.log(user);
+      console.log("token");
+      console.log(token);
+      const [google] = await prisma.account.findMany({
+        where: { userId: user.id, provider: "google" },
+      });
+      console.log("google");
+      console.log(google);
+      if (google.expires_at && google.expires_at * 1000 < Date.now()) {
+        console.log("hello");
+        // If the access token has expired, try to refresh it
+        try {
+          // https://accounts.google.com/.well-known/openid-configuration
+          // We need the `token_endpoint`.
+          const response = await fetch("https://oauth2.googleapis.com/token", {
+            headers: { "Content-Type": "application/x-www-form-urlencoded" },
+            body: new URLSearchParams({
+              client_id: process.env.GOOGLE_CLIENT_ID,
+              client_secret: process.env.GOOGLE_CLIENT_SECRET,
+              grant_type: "refresh_token",
+              refresh_token: google.refresh_token,
+            } as any),
+            method: "POST",
+          });
+
+          const tokens: TokenSet = await response.json();
+          console.log(tokens);
+
+          if (!response.ok) throw tokens;
+
+          await prisma.account.update({
+            data: {
+              access_token: tokens.access_token,
+              expires_at: Math.floor(
+                Date.now() / 1000 + (tokens.expires_in || 3599)
+              ),
+              refresh_token: tokens.refresh_token ?? google.refresh_token,
+            },
+            where: {
+              provider_providerAccountId: {
+                provider: "google",
+                providerAccountId: google.providerAccountId,
+              },
+            },
+          });
+        } catch (error) {
+          console.error("Error refreshing access token", error);
+          // The error property will be used client-side to handle the refresh token error
+          session.error = "RefreshAccessTokenError";
+        }
+      }
+      return {
+        ...session,
+        user: { ...session.user, id: user.id, role: user.role },
+      };
+    },
     async signIn({ user, account, profile, email, credentials }) {
       try {
-        if (email && !email.verificationRequest) {
+        // Oauth only
+        if (!email) {
+          return true;
+        }
+
+        if (!email.verificationRequest) {
           // TODO: Hash the URL to prevent direct access
           return "/errors/email-already-in-use?allowAccess=true";
         }
 
         const response = await fetch(
-          `${process.env.NEXTAUTH_URL}/api/users/me`,
+          `${process.env.NEXTAUTH_URL}/api/v1/users/me`,
           {
             method: "POST",
             headers: {
@@ -91,25 +175,22 @@ export const authOptions: NextAuthOptions = {
         const userEmail = await response.json();
 
         if (userEmail.exists) {
-          // TODO: Remove this code after development is complete.
-          if (!email) {
-            return true;
-          }
           // TODO: Hash the URL to prevent direct access
           return "/errors/email-already-in-use?allowAccess=true";
         }
 
-        return "/unauthorized";
+        return true;
       } catch (error) {
         console.error("An error occurred while checking the account.", error);
       }
 
-      return "/unauthorized";
+      return true;
     },
   },
   pages: {
     verifyRequest: "/auth/verify-request",
-    signIn: "/auth/signin",
+    // signIn: "/auth/signin",
+    newUser: "/auth/new-user",
   },
 };
 

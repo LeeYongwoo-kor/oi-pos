858bb67cb5059bef0f73ec00a3a08fab5d6538e0
LeeYongwoo-kor <newlyo1205@naver.com>
2023-06-12 23:05:27 +0900

    Nextauth api intermediate completion

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,8 +1,13 @@
 import {
   createAccountByNewProvider,
   getAccount,
+  getMenuItem,
+  getRestaurant,
+  getRestaurantTable,
+  getSubscription,
   getUserByEmail,
   getUserById,
+  updateSubscriptionStatus,
   updateUserRole,
 } from "@/database";
 import { deleteVerificationTokens } from "@/database/verificationToken";
@@ -12,8 +17,17 @@ import prisma from "@/lib/services/prismadb";
 import { sendEmail } from "@/lib/services/sendEmail";
 import { CustomError, ForbiddenError } from "@/lib/shared/CustomError";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
-import { Account, User, UserStatus } from "@prisma/client";
-import NextAuth, { NextAuthOptions, TokenSet } from "next-auth";
+import {
+  Account,
+  MenuItem,
+  Restaurant,
+  RestaurantTable,
+  Subscription,
+  SubscriptionStatus,
+  User,
+  UserStatus,
+} from "@prisma/client";
+import NextAuth, { NextAuthOptions, Session, TokenSet } from "next-auth";
 import EmailProvider from "next-auth/providers/email";
 import GoogleProvider from "next-auth/providers/google";
 import LineProvider from "next-auth/providers/line";
@@ -81,38 +95,10 @@ export const authOptions: NextAuthOptions = {
     // updateAge: 60 * 60 * 24, // 24 hours
   },
   jwt: {
-    maxAge: 60 * 60, // 1 hour
+    maxAge: 60 * 15, // 15 minutes
   },
   callbacks: {
     async jwt({ token, user, account, profile, isNewUser }) {
-      console.log("this is jwt");
-      // initial login success sign up
-      if (user) {
-        try {
-          const userInfo = await getUserById(user.id);
-          if (!userInfo || userInfo.status !== UserStatus.ACTIVE) {
-            throw new ForbiddenError(
-              "You are not active user. Please contact support team."
-            );
-          }
-          token = { ...token, status: userInfo.status, role: userInfo.role };
-        } catch (err) {
-          const errMessage =
-            err instanceof CustomError ? err.message : String(err);
-          //TODO: send error to sentry
-          console.error("Get user info failed: ", errMessage);
-          if (err instanceof CustomError) {
-            if (err instanceof ForbiddenError) {
-              return {
-                ...token,
-                errorName: "NotActiveUser",
-                errorMessage: errMessage,
-              };
-            }
-            return { ...token, errorName: err.name, errorMessage: errMessage };
-          }
-        }
-      }
       // new user registration
       if (isNewUser) {
         try {
@@ -125,11 +111,22 @@ export const authOptions: NextAuthOptions = {
           return {
             ...token,
             errorName: "UpdateUserError",
-            messageMessage: errMessage,
+            message: errMessage,
           };
         }
       }
-      // initial login with OAuth provider
+      // initial login success sign up
+      if (user) {
+        if (user.status !== UserStatus.ACTIVE) {
+          return {
+            ...token,
+            errorName: "NotActiveUser",
+            message: "You are not active user. Please contact support team.",
+          };
+        }
+        token = { ...token, status: user.status, role: user.role };
+      }
+      // initial login success sign up
       if (account) {
         if (profile && account.provider === "line") {
           token.name = profile.name;
@@ -145,136 +142,296 @@ export const authOptions: NextAuthOptions = {
           ),
           refresh_token: account.refresh_token,
         };
-      } else if (Date.now() < (token.expires_at || 0) * 1000) {
-        // If the access token has not expired yet, return it
-        return token;
       } else {
-        // If the access token has expired, try to refresh it
+        // Initialize token
+        const { message, redirectUrl, ...restToken } = token;
+        token = restToken;
         try {
-          let response: Response;
-
-          switch (token.provider) {
-            case "email":
-              // Email provider does not support refresh token
-              return token;
-            case "google":
-              if (!token.refresh_token) {
-                throw token;
-              }
-              // Refresh Google access token
-              response = await fetch("https://oauth2.googleapis.com/token", {
-                headers: {
-                  "Content-Type": "application/x-www-form-urlencoded",
-                },
-                body: new URLSearchParams({
-                  client_id: process.env.GOOGLE_CLIENT_ID!,
-                  client_secret: process.env.GOOGLE_CLIENT_SECRET!,
-                  grant_type: "refresh_token",
-                  refresh_token: token.refresh_token,
-                }),
-                method: "POST",
-              });
-              break;
-            case "line":
-              if (!token.refresh_token) {
-                throw token;
-              }
-              // Refresh Line access token
-              response = await fetch("https://api.line.me/oauth2/v2.1/token", {
-                headers: {
-                  "Content-Type": "application/x-www-form-urlencoded",
-                },
-                body: new URLSearchParams({
-                  client_id: process.env.LINE_CLIENT_ID!,
-                  client_secret: process.env.LINE_CLIENT_SECRET!,
-                  grant_type: "refresh_token",
-                  refresh_token: token.refresh_token,
-                }),
-                method: "POST",
-              });
-              break;
-            default:
-              return {
-                ...token,
-                errorName: "UnsupportedProviderError",
-                errorMessage: `Provider ${token.provider} is not supported`,
-              };
+          // user validation check from second token rotation
+          // 1. check user status
+          const userInfo = await withErrorRetry<User | null>(() =>
+            getUserById(token.sub)
+          )();
+          if (!userInfo || userInfo.status !== UserStatus.ACTIVE) {
+            throw new ForbiddenError(
+              "You are not active user. Please contact support team."
+            );
           }
 
-          const providerToken: TokenSet = await response.json();
-          if (!response.ok) throw providerToken;
+          // 2. check subscription status and check subscription info -> redirect plan register page
+          const subscriptionInfo = await withErrorRetry<Subscription | null>(
+            () => getSubscription(token.sub)
+          )();
+          if (!subscriptionInfo) {
+            return {
+              ...token,
+              message:
+                "You don't have subscription. Please register your subscription first.",
+              redirectUrl: "/plans/register",
+            };
+          }
 
-          return {
-            ...token, // Keep the previous token properties
-            access_token: providerToken.access_token,
-            expires_at: Math.floor(
-              Date.now() / 1000 + (providerToken.expires_in || 3599)
-            ),
-            // Fall back to old refresh token, but note that
-            // many providers may only allow using a refresh token once.
-            refresh_token: providerToken.refresh_token ?? token.refresh_token,
-          };
+          if (
+            subscriptionInfo.status === SubscriptionStatus.CANCELLED ||
+            subscriptionInfo.status === SubscriptionStatus.PENDING
+          ) {
+            return {
+              ...token,
+              message:
+                "Your subscription is cancelled or pending. Please register your subscription first.",
+              redirectUrl: "/plans/register",
+            };
+          }
+
+          if (subscriptionInfo.status === SubscriptionStatus.EXPIRED) {
+            return {
+              ...token,
+              message:
+                "Your subscription has expired. Please renew your subscription.",
+              redirectUrl: "/plans/register",
+            };
+          }
+
+          // 3. check subscription expiration date -> redirect plan register page
+          const currentDate = new Date();
+          const subscriptionEndDate = new Date(
+            subscriptionInfo.currentPeriodEnd
+          );
+          if (subscriptionEndDate < currentDate) {
+            await withErrorRetry<Subscription | null>(() =>
+              updateSubscriptionStatus(token.sub, SubscriptionStatus.EXPIRED)
+            )();
+            return {
+              ...token,
+              message:
+                "Your subscription has expired. Please renew your subscription.",
+              redirectUrl: "/plans/register",
+            };
+          }
+
+          // 4. check restraurant info -> redirect restaurant register page(step 1)
+          const restaurantInfo = await withErrorRetry<Restaurant | null>(() =>
+            getRestaurant(token.sub)
+          )();
+          if (!restaurantInfo) {
+            return {
+              ...token,
+              message:
+                "You don't register restaurant information yet. Please register restaurant information.",
+              redirectUrl: "/restaurants/info",
+            };
+          }
+
+          // 5. check restraurant info of startTime, endTime -> redirect hours register page(step 2)
+          if (!restaurantInfo.startTime || !restaurantInfo.endTime) {
+            return {
+              ...token,
+              message:
+                "You don't register restaurant hours. Please register restaurant hours.",
+              redirectUrl: "/restaurants/hours",
+            };
+          }
+
+          // 6. check restraurantTable info -> redirect table register page(step 3)
+          const restaurantTableInfo =
+            await withErrorRetry<RestaurantTable | null>(() =>
+              getRestaurantTable(restaurantInfo.id)
+            )();
+          if (!restaurantTableInfo) {
+            return {
+              ...token,
+              message:
+                "You don't register restaurant table. Please register restaurant table.",
+              redirectUrl: "/restaurants/table",
+            };
+          }
+
+          // 7. check menu info -> redirect menu register page(step 4)
+          const menuInfo = await withErrorRetry<MenuItem | null>(() =>
+            getMenuItem(restaurantInfo.id)
+          )();
+          if (!menuInfo) {
+            return {
+              ...token,
+              message: "You don't register menu. Please register menu.",
+              redirectUrl: "/menus",
+            };
+          }
+
+          return token;
         } catch (err) {
+          const errMessage =
+            err instanceof CustomError ? err.message : String(err);
           //TODO: send error to sentry
-          console.error("Error refreshing access token", err);
-          // The error property will be used client-side to handle the refresh token error
-          return {
-            ...token,
-            errorName: "RefreshAccessTokenError",
-            errorMessage: "Error refreshing access token",
-          };
+          console.error("Error occurred in token validation: ", errMessage);
+          if (err instanceof CustomError) {
+            if (err instanceof ForbiddenError) {
+              return {
+                ...token,
+                errorName: "NotActiveUser",
+                message: errMessage,
+              };
+            }
+            return { ...token, errorName: err.name, message: errMessage };
+          }
         }
       }
+
+      if (token?.provider === "email") {
+        // Email provider does not support refresh token
+        return token;
+      }
+
+      // for OAuth provider refresh token
+      if (token.refresh_token) {
+        if (Date.now() < (token.expires_at || 0) * 1000) {
+          // If the access token has not expired yet, return it
+          return token;
+        } else {
+          // If the access token has expired, try to refresh it
+          try {
+            let response: Response;
+            if (!token.refresh_token) {
+              throw token;
+            }
+
+            switch (token.provider) {
+              case "google":
+                // Refresh Google access token
+                response = await fetch("https://oauth2.googleapis.com/token", {
+                  headers: {
+                    "Content-Type": "application/x-www-form-urlencoded",
+                  },
+                  body: new URLSearchParams({
+                    client_id: process.env.GOOGLE_CLIENT_ID!,
+                    client_secret: process.env.GOOGLE_CLIENT_SECRET!,
+                    grant_type: "refresh_token",
+                    refresh_token: token.refresh_token,
+                  }),
+                  method: "POST",
+                });
+                break;
+              case "line":
+                // Refresh Line access token
+                response = await fetch(
+                  "https://api.line.me/oauth2/v2.1/token",
+                  {
+                    headers: {
+                      "Content-Type": "application/x-www-form-urlencoded",
+                    },
+                    body: new URLSearchParams({
+                      client_id: process.env.LINE_CLIENT_ID!,
+                      client_secret: process.env.LINE_CLIENT_SECRET!,
+                      grant_type: "refresh_token",
+                      refresh_token: token.refresh_token,
+                    }),
+                    method: "POST",
+                  }
+                );
+                break;
+              default:
+                return {
+                  ...token,
+                  errorName: "UnsupportedProviderError",
+                  message: `Provider ${token.provider} is not supported`,
+                };
+            }
+
+            const providerToken: TokenSet = await response.json();
+            if (!response.ok) throw providerToken;
+
+            return {
+              ...token, // Keep the previous token properties
+              access_token: providerToken.access_token,
+              expires_at: Math.floor(
+                Date.now() / 1000 + (providerToken.expires_in || 3599)
+              ),
+              // Fall back to old refresh token, but note that
+              // many providers may only allow using a refresh token once.
+              refresh_token: providerToken.refresh_token ?? token.refresh_token,
+            };
+          } catch (err) {
+            //TODO: send error to sentry
+            console.error("Error refreshing access token", err);
+            // The error property will be used client-side to handle the refresh token error
+            return {
+              ...token,
+              errorName: "RefreshAccessTokenError",
+              message: "Error refreshing access token",
+            };
+          }
+        }
+      }
+
+      return token;
     },
     async session({ session, user, token }) {
-      console.log("this is session");
-      const { errorName, errorMessage } = token;
+      const newSession: Partial<Session> = {
+        ...session,
+        user: {
+          ...session.user,
+          name: token?.name,
+          image: token?.picture,
+        },
+      };
+
       if (token) {
-        if (errorName) {
-          session.errorName = errorName;
-          session.errorMessage = errorMessage;
+        newSession.id = token.sub;
+        if (token.errorName) {
+          newSession.errorName = token.errorName;
         }
-        session.id = token.sub;
-        session.user.name = token.name;
-        session.user.image = token.picture;
+        if (token.message) {
+          newSession.message = token.message;
+        }
+        if (token.redirectUrl) {
+          newSession.redirectUrl = token.redirectUrl;
+        }
       }
-      return session;
+
+      return newSession as Session;
     },
     async signIn({ user, account, profile, email, credentials }) {
       try {
         if (email && !email.verificationRequest) {
-          // TODO: Hash the URL to prevent direct access
-          return "/errors/email-already-in-use?allowAccess=true";
+          // not verified email
+          return "/errors/EmailAlreadyInUse?allowAccess=true";
         }
 
-        const userByEmail = await getUserByEmail(user?.email);
+        const userByEmail = await withErrorRetry<User | null>(() =>
+          getUserByEmail(user?.email)
+        )();
+
         if (userByEmail) {
           if (email) {
-            // not email user
+            // not verified email
             if (!userByEmail.emailVerified) {
-              return "/errors/email-already-in-use?allowAccess=true";
+              return "/errors/EmailAlreadyInUse?allowAccess=true";
             }
 
             return true;
           }
 
-          const oauthAccount = await getAccount(
-            userByEmail.id,
-            account?.providerAccountId
-          );
+          const oauthAccount = await withErrorRetry<Account | null>(() =>
+            getAccount(userByEmail.id, account?.providerAccountId)
+          )();
 
           if (!oauthAccount) {
-            withErrorRetry<Account>(() =>
+            await withErrorRetry<Account>(() =>
               createAccountByNewProvider(userByEmail.id, account)
-            );
+            )();
           }
         }
 
         return true;
       } catch (err) {
+        const errMessage =
+          err instanceof CustomError ? err.message : String(err);
         //TODO: send error to sentry
-        console.error("An error occurred while checking the account: ", err);
-        return `/errors/prisma-error?allowAccess=true`;
+        console.error(
+          "An error occurred while checking the account: ",
+          errMessage
+        );
+        return `/errors/PrismaError?allowAccess=true`;
       }
     },
   },
@@ -284,7 +441,7 @@ export const authOptions: NextAuthOptions = {
       try {
         await deleteVerificationTokens(user?.email);
       } catch (err) {
-        //TODO: send error to sentry
+        //TODO: send error to sentry only
         console.error("Error deleting verification tokens: ", err);
       }
     },

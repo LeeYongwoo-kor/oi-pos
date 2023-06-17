45fac7699a51836ee45b87f91561deb403299490
LeeYongwoo-kor <newlyo1205@naver.com>
2023-06-17 23:56:32 +0900

    ※Security issue
    Modify api/users/me so that users who are not logged in cannot see it. Remove query parameters from endpoints so that only your own information is accessible

--- pages/api/v1/users/me.ts
+++ pages/api/v1/users/me.ts
@@ -2,15 +2,14 @@ import { getUserByEmail } from "@/database";
 import withApiHandler from "@/lib/server/withApiHandler";
 import { NotFoundError } from "@/lib/shared/CustomError";
 import { NextApiRequest, NextApiResponse } from "next";
+import { Session } from "next-auth";
 
-interface IGetUserQuery {
-  email: string;
-  [key: string]: string | string[] | undefined;
-}
-
-async function handler(req: NextApiRequest, res: NextApiResponse) {
-  const { email } = req.query as IGetUserQuery;
-  const existingUser = await getUserByEmail(email);
+async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse,
+  session?: Session | null
+) {
+  const existingUser = await getUserByEmail(session?.user.email);
   if (!existingUser) {
     throw new NotFoundError("User not found with the given email");
   }
@@ -21,5 +20,4 @@ async function handler(req: NextApiRequest, res: NextApiResponse) {
 export default withApiHandler({
   methods: ["GET"],
   handler,
-  isLoginRequired: false,
 });

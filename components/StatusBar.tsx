141564ad58c2e499d0f17484c94c0fe9dbf0a4b9
LeeYongwoo-kor <newlyo1205@naver.com>
2023-06-20 23:52:39 +0900

    1. Complete the info page 1st round 2. Add StatusBar component

---
+++ components/StatusBar.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+
+interface StatusBarProps {
+  steps: string[];
+  currentStep: string;
+}
+
+export const StatusBar: React.FC<StatusBarProps> = ({ steps, currentStep }) => {
+  const currentStepIndex = steps.indexOf(currentStep);
+
+  return (
+    <div className="flex justify-between">
+      {steps.map((step, index) => (
+        <div
+          key={step}
+          className={`relative flex-grow ${
+            index !== steps.length - 1 ? "mr-2" : ""
+          }`}
+        >
+          <div
+            className={`absolute top-2 left-2 w-6 h-6 flex items-center justify-center rounded-full z-10 ${
+              currentStepIndex >= index
+                ? "bg-blue-500 text-white"
+                : "bg-gray-200 text-gray-500"
+            }`}
+          >
+            {index + 1}
+          </div>
+          <div
+            className={`absolute top-5 w-full h-1 ${
+              currentStepIndex >= index ? "bg-blue-500" : "bg-gray-200"
+            }`}
+          />
+          <div className="mt-8 text-center">{step}</div>
+        </div>
+      ))}
+    </div>
+  );
+};
--- pages/restaurants/info.tsx
+++ pages/restaurants/info.tsx
@@ -1,24 +1,121 @@
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
 import Layout from "@/components/Layout";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import useSWR, { useSWRConfig } from "swr";
+import useSubmit from "@/hooks/useSubmit";
+import { useToast } from "@/hooks/useToast";
+import { StatusBar } from "@/components/StatusBar";
+
+interface IAddress {
+  address1: string;
+  address2: string;
+  address3: string;
+  kana1: string;
+  kana2: string;
+  kana3: string;
+  prefcode: string;
+  zipcode: string;
+}
+
+interface ZipcodeApiResponse {
+  message: string | null;
+  results: IAddress[] | null;
+  status: number;
+}
+
+const schema = yup.object().shape({
+  restaurantName: yup
+    .string()
+    .required("Restaurant Name is required")
+    .max(30, "Restaurant Name should be at most 30 characters")
+    .matches(/^\s*\w.*$/, "No special characters allowed"),
+  branchName: yup
+    .string()
+    .required("Branch Name is required")
+    .max(20, "Branch Name should be at most 20 characters")
+    .matches(/^\s*\w.*$/, "No special characters allowed"),
+  phone: yup.string().matches(/^0\d{9,10}$/, "Must be a valid phone number"),
+  postCode: yup
+    .string()
+    .required("Post Code is required")
+    .matches(/^\d{7}$/, "Must be a valid post code"),
+  address: yup
+    .string()
+    .required("Address is required. Please input post code and click search"),
+  restAddress: yup
+    .string()
+    .max(50, "Rest Address should be at most 50 characters")
+    .matches(
+      /^[a-zA-Z0-9\s,.'-]*$/,
+      "No special characters allowed other than address characters"
+    ),
+});
 
 export default function RestaurantInfo() {
-  const [restaurantName, setRestaurantName] = useState("");
-  const [branchName, setBranchName] = useState("");
-  const [phone, setPhone] = useState("");
-  const [postCode, setPostCode] = useState("");
-  const [address, setAddress] = useState("");
-  const [restAddress, setRestAddress] = useState("");
+  const {
+    register,
+    handleSubmit: formSubmit,
+    setValue,
+    watch,
+    resetField,
+    formState: { errors, isValid, touchedFields },
+  } = useForm({
+    resolver: yupResolver(schema),
+    mode: "onChange",
+  });
+  const watchFields = watch();
+  // const [isSubmitting, setIsSubmitting] = useState(false);
+  const [isSearchButtonEnabled, setSearchButtonEnabled] = useState(false);
+  const { addToast } = useToast();
+  const { mutate } = useSWRConfig();
+  const { data, error } = useSWR<ZipcodeApiResponse>(
+    isSearchButtonEnabled
+      ? `https://zip-cloud.appspot.com/api/search?zipcode=${watchFields.postCode}`
+      : null
+  );
+
+  const onSubmitCallback = async (formData: any) => {
+    console.log(formData);
+  };
+  const { isSubmitting, handleSubmit } = useSubmit(onSubmitCallback);
 
   const handlePostCodeSearch = () => {
-    // Call your postcode API here and set the address
+    if (!watchFields.postCode) return;
+    resetField("address");
+    mutate(
+      `https://zip-cloud.appspot.com/api/search?zipcode=${watchFields.postCode}`
+    );
   };
 
-  const handleNext = () => {
-    // Implement your next logic here
-  };
+  useEffect(() => {
+    setSearchButtonEnabled(!errors.postCode && !!watchFields.postCode);
+  }, [errors.postCode, watchFields.postCode]);
+
+  useEffect(() => {
+    if (isSearchButtonEnabled) {
+      if (data?.results === null) {
+        addToast("error", "Post Code not found, Please try again");
+        resetField("address");
+      }
+      if (data?.results && data.results.length > 0) {
+        const addressResult = [
+          data.results[0].address1,
+          data.results[0].address2,
+          data.results[0].address3,
+        ].join(" ");
+        setValue("address", addressResult);
+      }
+    }
+  }, [data, setValue]);
 
   return (
     <Layout>
+      <StatusBar
+        steps={["Info", "Hours", "Tables", "Menus", "Complete"]}
+        currentStep="Tables"
+      />
       <div className="container px-4 py-8 mx-auto">
         <h1 className="mb-4 text-2xl font-semibold">
           Enter basic information about the restaurant
@@ -26,76 +123,146 @@ export default function RestaurantInfo() {
         <p className="mb-6">
           This page helps you enter basic information about your restaurant.
         </p>
-        <form>
+        <form onSubmit={formSubmit(handleSubmit)}>
           <div className="mb-4">
-            <label className="block mb-2">Restaurant Name (Required)</label>
+            <label className="block mb-2">Restaurant Name</label>
             <input
-              className="w-full p-2 border border-gray-300 rounded"
+              className={`w-full p-2 border ${
+                errors.restaurantName
+                  ? "border-red-500"
+                  : touchedFields.restaurantName && watchFields.restaurantName
+                  ? "border-green-500"
+                  : "border-gray-400"
+              } rounded`}
               type="text"
-              value={restaurantName}
-              onChange={(e) => setRestaurantName(e.target.value)}
-              required
+              {...register("restaurantName")}
             />
+            {errors.restaurantName && (
+              <span className="text-red-500">
+                {errors.restaurantName?.message as string}
+              </span>
+            )}
           </div>
           <div className="mb-4">
-            <label className="block mb-2">Branch Name (Required)</label>
+            <label className="block mb-2">Branch Name</label>
             <input
-              className="w-full p-2 border border-gray-300 rounded"
+              className={`w-full p-2 border ${
+                errors.branchName
+                  ? "border-red-500"
+                  : touchedFields.branchName && watchFields.branchName
+                  ? "border-green-500"
+                  : "border-gray-400"
+              } rounded`}
               type="text"
-              value={branchName}
-              onChange={(e) => setBranchName(e.target.value)}
-              required
+              {...register("branchName")}
             />
+            {errors.branchName && (
+              <span className="text-red-500">
+                {errors.branchName?.message as string}
+              </span>
+            )}
           </div>
           <div className="mb-4">
             <label className="block mb-2">
-              Restaurant Phone Number (Not required)
+              Restaurant Phone Number (Please omit the -)
             </label>
             <input
-              className="w-full p-2 border border-gray-300 rounded"
+              className={`w-full p-2 border ${
+                errors.phone
+                  ? "border-red-500"
+                  : touchedFields.phone && watchFields.phone
+                  ? "border-green-500"
+                  : "border-gray-400"
+              } rounded`}
               type="tel"
-              value={phone}
-              onChange={(e) => setPhone(e.target.value)}
+              maxLength={11}
+              {...register("phone")}
             />
+            {errors.phone && (
+              <span className="text-red-500">
+                {errors.phone?.message as string}
+              </span>
+            )}
           </div>
-          <div className="mb-4">
-            <label className="block mb-2">POST CODE (Required)</label>
+          <label className="block mb-2">
+            Japan Postal Code (Please omit the -)
+          </label>
+          <div className="flex items-center">
             <input
-              className="p-2 border border-gray-300 rounded"
+              className={`w-full p-2 border ${
+                errors.postCode
+                  ? "border-red-500"
+                  : touchedFields.postCode && watchFields.postCode
+                  ? "border-green-500"
+                  : "border-gray-400"
+              } rounded`}
               type="text"
-              value={postCode}
-              onChange={(e) => setPostCode(e.target.value)}
-              required
+              maxLength={7}
+              {...register("postCode")}
             />
             <button
-              className="p-2 ml-2 text-white bg-blue-500 rounded"
+              className={`p-2 ml-2 text-white bg-blue-500 rounded ${
+                isSearchButtonEnabled
+                  ? "hover:bg-blue-700"
+                  : "opacity-50 cursor-not-allowed"
+              }`}
+              type="button"
               onClick={handlePostCodeSearch}
+              disabled={!isSearchButtonEnabled}
             >
               Search
             </button>
           </div>
-          <div className="mb-4">
+          {errors.postCode && (
+            <span className="text-red-500">
+              {errors.postCode?.message as string}
+            </span>
+          )}
+          <div className="mt-4 mb-4">
             <label className="block mb-2">Address</label>
             <input
-              className="w-full p-2 border border-gray-300 rounded"
+              className={`w-full p-2 border ${
+                errors.address
+                  ? "border-red-500"
+                  : touchedFields.address
+                  ? "border-green-500"
+                  : "border-gray-400"
+              } rounded`}
               type="text"
-              value={address}
-              onChange={(e) => setAddress(e.target.value)}
+              {...register("address")}
               disabled
             />
+            {errors.address && (
+              <span className="text-red-500">
+                {errors.address?.message as string}
+              </span>
+            )}
           </div>
           <div className="mb-4">
-            <label className="block mb-2">Rest of the Address</label>
+            <label className="block mb-2">
+              Rest of the Address (Not required)
+            </label>
             <input
-              className="w-full p-2 border border-gray-300 rounded"
+              className={`w-full p-2 border ${
+                errors.restAddress ? "border-red-500" : "border-green-500"
+              } rounded`}
               type="text"
-              value={restAddress}
-              onChange={(e) => setRestAddress(e.target.value)}
+              {...register("restAddress")}
             />
+            {errors.restAddress && (
+              <span className="text-red-500">
+                {errors.restAddress?.message as string}
+              </span>
+            )}
           </div>
           <button
-            className="p-2 text-white bg-green-500 rounded"
-            onClick={handleNext}
+            className={`p-2 text-white bg-green-600 rounded ${
+              isValid && !isSubmitting
+                ? "hover:bg-green-700"
+                : "opacity-60 cursor-not-allowed"
+            }}`}
+            type="submit"
+            disabled={!isValid || isSubmitting}
           >
             NEXT
           </button>

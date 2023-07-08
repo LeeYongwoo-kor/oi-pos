0af36fd1dcda8e3715bc87c5b2b1a989ef0431a0
LeeYongwoo-kor <newlyo1205@naver.com>
2023-07-08 13:31:09 +0900

    * Remove the fatal bug in sendEmail (Bug No. 12) 1. Modify the import of hasNullUndefined to not refer to default 2. Invalid!hasNullUndefined decision removed

--- lib/services/sendEmail.ts
+++ lib/services/sendEmail.ts
@@ -1,6 +1,6 @@
-import hasNullUndefined from "@/utils/checkNullUndefined";
+import { hasNullUndefined } from "@/utils/checkNullUndefined";
 import sgMail from "@sendgrid/mail";
-import { UnexpectedError, ValidationError } from "../shared/CustomError";
+import { UnexpectedError, ValidationError } from "../shared/ApiError";
 
 sgMail.setApiKey(process.env.SENDGRID_API_KEY!);
 
@@ -20,7 +20,7 @@ export async function sendEmail({ to, from, subject, html }: IEmailParams) {
   };
 
   try {
-    if (!hasNullUndefined({ to, from, subject, html })) {
+    if (hasNullUndefined({ to, from, subject, html })) {
       throw new ValidationError("Failed to send email. Please try again later");
     }
 
@@ -32,7 +32,7 @@ export async function sendEmail({ to, from, subject, html }: IEmailParams) {
     //TODO: send error to sentry
     console.error(err);
     if (err instanceof ValidationError) {
-      throw err;
+      throw err.message;
     }
     const unexpectedError = err instanceof Error ? err : new Error(String(err));
     throw new UnexpectedError(unexpectedError.message);

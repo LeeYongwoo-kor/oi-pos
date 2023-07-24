460bf19c1c7e1b4891e428da7bffd3a9af87b63b
LeeYongwoo-kor <newlyo1205@naver.com>
2023-07-24 23:54:28 +0900

    1. Rename the endpoint of api/users/me to api/me/users 2. Add api/me/restaurants! Comment: When you bring your own information, you bring it from me! By default, it is not imported from api/restaurants/{userId}.

---
+++ pages/api/v1/me/restaurants.ts
@@ -0,0 +1,25 @@
+import { getRestaurantAllInfo } from "@/database";
+import withApiHandler from "@/lib/server/withApiHandler";
+import { NotFoundError } from "@/lib/shared/ApiError";
+import { NextApiRequest, NextApiResponse } from "next";
+import { Session } from "next-auth";
+
+async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse,
+  session?: Session | null
+) {
+  const restaurant = await getRestaurantAllInfo(session?.id);
+  if (!restaurant) {
+    throw new NotFoundError(
+      "Restaurant not found. Please create restaurant first"
+    );
+  }
+
+  return res.status(200).json(restaurant);
+}
+
+export default withApiHandler({
+  methods: ["GET"],
+  handler,
+});

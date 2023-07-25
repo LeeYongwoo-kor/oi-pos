70cfeb9547396b2a5f70577cb9ae21523399ab4e
LeeYongwoo-kor <newlyo1205@naver.com>
2023-07-25 23:59:29 +0900

    * Improvement of converDatesToISOString utility function (Bug No.15)

--- utils/convertDatesToISOString.ts
+++ utils/convertDatesToISOString.ts
@@ -1,19 +1,26 @@
 export default function convertDatesToISOString<T extends Record<string, any>>(
   obj: T
 ): T {
-  const convertedObj: Record<string, any> = {};
+  if (typeof obj !== "object" || obj === null) {
+    return obj;
+  }
 
-  for (const [key, value] of Object.entries(obj)) {
-    if (typeof value === "object" && value !== null) {
-      if (value instanceof Date) {
-        convertedObj[key] = value.toISOString();
-      } else {
-        convertedObj[key] = convertDatesToISOString(value);
-      }
-    } else {
-      convertedObj[key] = value;
+  const newObj: Record<string, any> = { ...obj };
+
+  for (const [key, _] of Object.entries(newObj)) {
+    if (newObj[key] instanceof Date) {
+      newObj[key] = newObj[key].toISOString();
+    } else if (Array.isArray(newObj[key])) {
+      newObj[key] = newObj[key].map((item: any) => {
+        if (typeof item === "object" && item !== null) {
+          return convertDatesToISOString(item);
+        }
+        return item;
+      });
+    } else if (typeof newObj[key] === "object" && newObj[key] !== null) {
+      newObj[key] = convertDatesToISOString(newObj[key]);
     }
   }
 
-  return convertedObj as T;
+  return newObj as T;
 }

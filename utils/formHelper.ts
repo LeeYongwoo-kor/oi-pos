247c667d01797205bce721e4c11a5fb6420d5aa9
LeeYongwoo-kor <newlyo1205@naver.com>
2023-08-10 21:55:38 +0900

    1. If any of the comparison targets is empty in the isFormChanged utility, it is determined that the value of the form has changed. 2. Change the isEmpty utility to ensure that the array (although it is a kind of object) is empty as well as the object

--- utils/formHelper.ts
+++ utils/formHelper.ts
@@ -1,9 +1,12 @@
 import { FieldValues } from "react-hook-form";
+import isEmpty from "./validation/isEmpty";
 
 export const isFormChanged = <T extends Record<string, any>>(
   originData: T,
   formData: FieldValues
 ): boolean => {
+  if (isEmpty(formData) || isEmpty(originData)) return true;
+
   return Object.keys(formData).some((key) => {
     return originData[key] !== formData[key];
   });
--- utils/validation/isEmpty.ts
+++ utils/validation/isEmpty.ts
@@ -1,3 +1,8 @@
-export default function isEmpty(obj: Record<string, any> | null | undefined) {
-  return !obj || Object.keys(obj).length === 0;
+export default function isEmpty(
+  input: Record<string, any> | any[] | null | undefined
+) {
+  if (Array.isArray(input)) {
+    return input.length === 0;
+  }
+  return !input || Object.keys(input).length === 0;
 }

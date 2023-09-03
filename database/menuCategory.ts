7cc12c288370dc1c038c6fe9a8a3b5f4af8c0ae4
LeeYongwoo-kor <newlyo1205@naver.com>
2023-09-03 23:56:26 +0900

    Complete menuCategory database logic (Add upsert, update, delete)

--- database/menuCategory.ts
+++ database/menuCategory.ts
@@ -11,11 +11,22 @@ export interface IMenuCategory extends MenuCategory {
 export interface CreateMenuCategoryParams {
   restaurantId: string;
   name: string;
-  imageUrl: string;
+  imageUrl?: string;
+  description?: string;
+  displayOrder?: number;
+}
+
+export interface UpdateMenuCategoryParams {
+  id: string;
+  name: string;
+  imageUrl?: string;
   imageVersion?: number;
   description?: string;
   displayOrder?: number;
 }
+
+export type UpsertMenuCategoryParams = CreateMenuCategoryParams &
+  UpdateMenuCategoryParams;
 
 export async function getAllCategoriesByRestaurantId(
   restaurantId: string | null | undefined
@@ -54,10 +65,80 @@ export async function createMenuCategory(
         name: menuCategryInfo.name,
         description: menuCategryInfo.description,
         imageUrl: menuCategryInfo.imageUrl,
-        imageVersion: menuCategryInfo.imageVersion,
         displayOrder: menuCategryInfo.displayOrder,
       },
     }),
     "createMenuCategory"
+  );
+}
+
+export async function upsertMenuCategory(
+  menuCategryInfo: UpsertMenuCategoryParams
+): Promise<MenuCategory> {
+  if (hasNullUndefined(menuCategryInfo)) {
+    throw new ValidationError(
+      "Failed to create menu category. Please try again later"
+    );
+  }
+
+  const upsertMenuCategoryData = {
+    id: menuCategryInfo.id,
+    restaurantId: menuCategryInfo.restaurantId,
+    name: menuCategryInfo.name,
+    description: menuCategryInfo.description,
+    imageUrl: menuCategryInfo.imageUrl,
+    imageVersion: menuCategryInfo.imageVersion,
+    displayOrder: menuCategryInfo.displayOrder,
+  };
+
+  return prismaRequestHandler(
+    prisma.menuCategory.upsert({
+      where: {
+        id: menuCategryInfo.id,
+      },
+      create: upsertMenuCategoryData,
+      update: upsertMenuCategoryData,
+    }),
+    "createMenuCategory"
+  );
+}
+
+export async function updateMenuCategory(
+  menuCategoryId: string | null | undefined,
+  updateCategoryInfo: Partial<Omit<MenuCategory, "id" | "restaurantId">>
+): Promise<MenuCategory> {
+  if (!menuCategoryId || hasNullUndefined(updateCategoryInfo)) {
+    throw new ValidationError(
+      "Failed to update menu category. Please try again later"
+    );
+  }
+
+  return prismaRequestHandler(
+    prisma.menuCategory.update({
+      where: {
+        id: menuCategoryId,
+      },
+      data: updateCategoryInfo,
+    }),
+    "updateMenuCategory"
+  );
+}
+
+export async function deleteMenuCategory(
+  menuCategoryId: string | null | undefined
+): Promise<MenuCategory> {
+  if (!menuCategoryId) {
+    throw new ValidationError(
+      "Failed to delete menu category. Please try again later"
+    );
+  }
+
+  return prismaRequestHandler(
+    prisma.menuCategory.delete({
+      where: {
+        id: menuCategoryId,
+      },
+    }),
+    "deleteMenuCategory"
   );
 }

6de9db0feeada18b97fc6317212ced459b1863f1
LeeYongwoo-kor <newlyo1205@naver.com>
2023-09-06 20:36:16 +0900

    1. Add awsS3CheckIfFolderExists to check if folder keys exist for the root of AWS S3 2. add platter image (static), change path to images 3. In conversDatesToISOSringutil, if an array exists in the value of the object internal key in the form of an array, troubleshoot the error that appears as an empty array (Bug No.62)

---
+++ lib/server/awsS3CheckIfFolderExists.ts
@@ -0,0 +1,21 @@
+import { ListObjectsV2Command } from "@aws-sdk/client-s3";
+import s3 from "../services/awsS3";
+import { AWS_S3_YOSHI_BUCKET } from "@/constants/service";
+
+export default async function awsS3CheckIfFolderExists(
+  folderKey: string
+): Promise<boolean> {
+  const normalizedFolderKey = folderKey.endsWith("/")
+    ? folderKey
+    : `${folderKey}/`;
+
+  const listObjectsCommand = new ListObjectsV2Command({
+    Bucket: AWS_S3_YOSHI_BUCKET,
+    Prefix: normalizedFolderKey,
+    Delimiter: "/",
+    MaxKeys: 1,
+  });
+
+  const listObjectsResponse = await s3.send(listObjectsCommand);
+  return (listObjectsResponse.Contents?.length ?? 0) > 0;
+}
--- utils/converter/convertDatesToISOString.ts
+++ utils/converter/convertDatesToISOString.ts
@@ -1,18 +1,18 @@
-export default function convertDatesToISOString<T extends Record<string, any>>(
-  obj: T
-): T {
+export default function convertDatesToISOString<
+  T extends Record<string, any> | null
+>(obj: T): T {
   if (typeof obj !== "object" || obj === null) {
     return obj;
   }
 
   const isArrayLike = Array.isArray(obj);
-  const newObj: Record<string, any> = isArrayLike ? [] : { ...obj };
+  const newObj: Record<string, any> = isArrayLike ? [...obj] : { ...obj };
 
   for (const [key, _] of Object.entries(newObj)) {
     if (newObj[key] instanceof Date) {
       newObj[key] = newObj[key].toISOString();
     } else if (Array.isArray(newObj[key])) {
-      newObj[key] = newObj[key].map((item: any) => {
+      newObj[key] = [...newObj[key]].map((item: any) => {
         if (typeof item === "object" && item !== null) {
           return convertDatesToISOString(item);
         }

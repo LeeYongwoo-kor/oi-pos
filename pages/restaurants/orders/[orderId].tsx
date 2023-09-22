dd376db8013d50d40cb494cdf1d1b752fec02d1c
LeeYongwoo-kor <newlyo1205@naver.com>
2023-09-22 22:25:44 +0900

    1. Complete (Setup) Page 1st Complete 2. [orderId] Page 2nd Complete

--- pages/restaurants/orders/[orderId].tsx
+++ pages/restaurants/orders/[orderId].tsx
@@ -1,13 +1,70 @@
+import Menu from "@/components/menu/Menu";
 import { COMMON_ERROR } from "@/constants/errorMessage/client";
-import { getActiveOrderById } from "@/database";
+import { ERROR_URL } from "@/constants/url";
+import { IOrder, getActiveOrderById } from "@/database";
+import { useToast } from "@/hooks/useToast";
 import { ApiError, NotFoundError } from "@/lib/shared/error/ApiError";
+import { IOrderInfo, orderInfoState } from "@/recoil/state/orderState";
+import convertDatesToISOString from "@/utils/converter/convertDatesToISOString";
 import { GetServerSidePropsContext } from "next";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+import { useSetRecoilState } from "recoil";
 
-export default function Order({ order }: any) {
-  console.log(order);
+type OrderPageProps = {
+  order: IOrder | undefined;
+  initErrMsg: string | undefined;
+  notFound: boolean | undefined;
+};
+
+export default function Order({ order, initErrMsg, notFound }: OrderPageProps) {
+  const router = useRouter();
+  const { addToast } = useToast();
+  const setOrderInfo = useSetRecoilState(orderInfoState);
+
+  useEffect(() => {
+    if (order) {
+      const { id, status, table } = order;
+      const orderInfo: IOrderInfo = {
+        orderId: id,
+        orderStatus: status,
+        tableId: table.id,
+        tableNumber: table.number,
+      };
+      setOrderInfo(orderInfo);
+    }
+  }, [order]);
+
+  useEffect(() => {
+    const hasErrors = async () => {
+      if (initErrMsg) {
+        await router.replace(ERROR_URL.SERVER_ERROR);
+        addToast("error", initErrMsg);
+      }
+      if (notFound) {
+        await router.replace(ERROR_URL.NOT_FOUND);
+        addToast(
+          "error",
+          "Not found restaurant table. Please contact the staff"
+        );
+      }
+    };
+    hasErrors();
+  }, [initErrMsg, notFound]);
+
   return (
-    <div>
-      <h1>This is Order Page</h1>
+    <div className="">
+      <div className="flex justify-between text-sm font-light text-white bg-slate-700">
+        <span>
+          🍔{order?.table?.tableType} No: {order?.table?.number}
+        </span>
+        <div className="flex space-x-1">
+          <span>Open: {order?.table?.restaurant?.startTime}</span>
+          <span>~ {order?.table?.restaurant?.endTime}</span>
+        </div>
+        <span>Last Order: {order?.table?.restaurant?.lastOrder}</span>
+      </div>
+      <Menu restaurantInfo={order?.table?.restaurant} role={"user"} />
     </div>
   );
 }
@@ -20,7 +77,19 @@ export async function getServerSideProps(ctx: GetServerSidePropsContext) {
     }
 
     const decodedOrderId = Buffer.from(orderId, "base64").toString("utf-8");
-    const order = await getActiveOrderById(decodedOrderId);
+    const order = convertDatesToISOString(
+      await getActiveOrderById(decodedOrderId)
+    );
+
+    console.log("order", order);
+
+    if (!order) {
+      return {
+        props: {
+          notFound: true,
+        },
+      };
+    }
 
     return {
       props: { order },
--- pages/restaurants/setup/complete.tsx
+++ pages/restaurants/setup/complete.tsx
@@ -1,3 +1,41 @@
-export default function Complete() {
-  return <div>This is Complete Page</div>;
+import Layout from "@/components/Layout";
+import { StatusBar } from "@/components/StatusBar";
+import { RESTAURANT_SETUP_STEPS } from "@/constants/status";
+import { RESTAURANT_URL } from "@/constants/url";
+import { useRouter } from "next/router";
+
+export default function SetupComplete() {
+  const router = useRouter();
+
+  const handlePrevious = () => {
+    router.push(RESTAURANT_URL.SETUP.MENUS);
+  };
+  return (
+    <Layout>
+      <StatusBar steps={RESTAURANT_SETUP_STEPS} currentStep="Complete" />
+      <div className="flex flex-col items-center justify-center h-[46rem] bg-gray-100">
+        <div className="p-8 bg-white border rounded-lg shadow-lg">
+          <div className="flex flex-col items-center">
+            <div className="mb-4 text-2xl font-semibold">
+              Your Service is Ready!
+            </div>
+            <div className="mt-4 mb-8 text-lg">
+              All information is entered and you&apos;re good to go.
+            </div>
+            <div className="flex space-x-2">
+              <button
+                onClick={handlePrevious}
+                className="px-8 py-2 text-white rounded-full bg-sky-500 hover:bg-sky-600"
+              >
+                Previous
+              </button>
+              <button className="px-8 py-2 text-lg font-semibold text-white bg-green-600 rounded-full hover:bg-green-600">
+                Start
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Layout>
+  );
 }

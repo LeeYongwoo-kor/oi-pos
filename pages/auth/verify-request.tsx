9e1eb83fe87bec0ac732f9638d0acc7ee75cd99a
LeeYongwoo-kor <newlyo1205@naver.com>
2023-10-02 23:56:15 +0900

    Completion of the verifyRequest page, the mail verification page

--- pages/auth/verify-request.tsx
+++ pages/auth/verify-request.tsx
@@ -1,4 +1,14 @@
 export default function VerifyRequest() {
-  console.log("This is VerifyRequest Callback");
-  return <div>Please check your Email!</div>;
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+      <div className="p-8 bg-white rounded-lg shadow-md w-96">
+        <h1 className="mb-4 text-2xl font-semibold text-center">
+          Email Verification
+        </h1>
+        <p className="text-center text-gray-700">
+          Please check your email to verify your account.
+        </p>
+      </div>
+    </div>
+  );
 }
